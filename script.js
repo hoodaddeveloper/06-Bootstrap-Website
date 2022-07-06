e63dcd01b0707423292ed9edef24ea6a0875b1a4
Mohammad Khafas <mohammadkhaffas@gmail.com>
2022-07-06 14:28:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,20 +10,22 @@ form.onsubmit = (e)=>{
   let xhr = new XMLHttpRequest();
   xhr.open("POST", "message.php", true);
   xhr.onload = ()=>{
-    if(xhr.readyState == 4 && xhr.status == 200){
+    if (xhr.readyState == 4 && xhr.status == 200) {
       let response = xhr.response;
-      if(response.indexOf("required") != -1 || response.indexOf("valid") != -1 || response.indexOf("failed") != -1){
+      if (response.indexOf("required") != -1 || response.indexOf("valid") != -1 || response.indexOf("failed") != -1) {
         statusTxt.style.color = "red";
-      }else{
+      } else {
         form.reset();
         setTimeout(()=>{
           statusTxt.style.display = "none";
         }, 3000);
       }
+      
       statusTxt.innerText = response;
       form.classList.remove("disabled");
     }
   }
+  
   let formData = new FormData(form);
   xhr.send(formData);
 }
