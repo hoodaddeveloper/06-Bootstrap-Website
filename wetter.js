4701debb1730dab285c5d731c6c7e7eb63d9d59f
Mohammad Khafas <mohammadkhaffas@gmail.com>
2022-07-06 14:24:28 +0200

    Update wetter.js

--- wetter.js
+++ wetter.js
@@ -16,11 +16,11 @@ async function getWeather(city) {
   app.innerHTML = `
     <div class="card">
       <div class="wetter"><i style="font-size:3rem;" class="wi ${today.symbol}"></i></div>
-      <div class="card-body text-center"><h2>${City}</h2><small class="my-3 d-block">${today.condition}</small><div>
-        <button class="btn btn-danger">${today.max}</button>
-        <button class="btn btn-secondary">${now_temp}</button>
-        <button class="btn btn-primary">${today.min}</button>
-      </div>
+        <div class="card-body text-center"><h2>${City}</h2><small class="my-3 d-block">${today.condition}</small><div>
+          <button class="btn btn-danger">${today.max}</button>
+          <button class="btn btn-secondary">${now_temp}</button>
+          <button class="btn btn-primary">${today.min}</button>
+        </div>
       </div>
     </div>`
 }
