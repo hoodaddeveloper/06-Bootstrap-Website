9c6e52efa0a700f06d61f2050e8bc3588a765e2f
Mohammad Khafas <mohammadkhaffas@gmail.com>
2022-07-06 14:22:17 +0200

    Update wetter.js

--- wetter.js
+++ wetter.js
@@ -13,17 +13,16 @@ async function getWeather(city) {
   let now_temp = data.result.hava.summary.temp
   let today = data.result.hava.dayList[0]
   
-    app.innerHTML = `
-      <div class="card">
-        <div class="wetter"><i style="font-size:3rem;" class="wi ${today.symbol}"></i></div>
-        <div class="card-body text-center"><h2>${City}</h2><small class="my-3 d-block">${today.condition}</small><div>
-                    <button class="btn btn-danger">${today.max}</button>
-                    <button class="btn btn-secondary">${now_temp}</button>
-                    <button class="btn btn-primary">${today.min}</button>
-                </div>
-            </div>
-        </div>
-    `
+  app.innerHTML = `
+    <div class="card">
+      <div class="wetter"><i style="font-size:3rem;" class="wi ${today.symbol}"></i></div>
+      <div class="card-body text-center"><h2>${City}</h2><small class="my-3 d-block">${today.condition}</small><div>
+        <button class="btn btn-danger">${today.max}</button>
+        <button class="btn btn-secondary">${now_temp}</button>
+        <button class="btn btn-primary">${today.min}</button>
+      </div>
+      </div>
+    </div>`
 }
 
 getWeather ("تهران")
