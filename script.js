3469fc5671c9de2d1be9c10256a0d44829435d26
Mohammad Khafas <mohammadkhaffas@gmail.com>
2022-07-07 16:09:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const form = document.querySelector("form"),
 statusTxt = form.querySelector(".button-area span");
-form.onsubmit = (e)=>{
+form.onsubmit = (e) => {
   e.preventDefault();
   statusTxt.style.color = "#0D6EFD";
   statusTxt.style.display = "block";
@@ -9,14 +9,14 @@ form.onsubmit = (e)=>{
 
   let xhr = new XMLHttpRequest();
   xhr.open("POST", "message.php", true);
-  xhr.onload = ()=>{
+  xhr.onload = () => {
     if (xhr.readyState == 4 && xhr.status == 200) {
       let response = xhr.response;
       if (response.indexOf("required") != -1 || response.indexOf("valid") != -1 || response.indexOf("failed") != -1) {
         statusTxt.style.color = "red";
       } else {
         form.reset();
-        setTimeout(()=>{
+        setTimeout(() => {
           statusTxt.style.display = "none";
         }, 3000);
       }
