f818d34efc56e0e4a67856124c46c6a720102880
Mohammad Khafas <mohammadkhaffas@gmail.com>
2022-10-20 20:25:53 +0200

    Update message.php

--- message.php
+++ message.php
@@ -5,21 +5,21 @@
   $website = htmlspecialchars($_POST['website']);
   $message = htmlspecialchars($_POST['message']);
 
-  if(!empty($email) && !empty($message)){
-    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+  if (!empty($email) && !empty($message)) {
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
       $receiver = "hoodad94@gmail.com";
       $subject = "From: $name <$email>";
       $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\n\nMessage:\n$message\n\nRegards,\n$name";
       $sender = "From: $email";
-      if(mail($receiver, $subject, $body, $sender)){
+      if (mail($receiver, $subject, $body, $sender)) {
          echo "Ihre Nachricht wurde gesendet";
-      }else{
+      } else {
          echo "Sorry, Ihre Nachricht konnte nicht gesendet werden!";
       }
-    }else{
+    } else {
       echo "Geben Sie eine gültige E-Mail-Adresse an!";
     }
-  }else{
+  } else {
     echo "E-Mail- und Nachrichtenfeld sind erforderlich!";
   }
 ?>
